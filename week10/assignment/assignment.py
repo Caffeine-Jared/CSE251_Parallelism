77e7f651dc4e48f8a66607aaf4dc3dce38e069d1
Caffeine-Jared <51540063+Caffeine-Jared@users.noreply.github.com>
2023-12-13 19:40:30 -0700

    update

--- week10/assignment/assignment.py
+++ week10/assignment/assignment.py
@@ -1,61 +1,164 @@
-"""
-Course: CSE 251
-Lesson Week: 10
-File: assignment.py
-Author: <your name>
+# """
+# Course: CSE 251
+# Lesson Week: 10
+# File: assignment.py
+# Author: <your name>
 
-Purpose: assignment for week 10 - reader writer problem
+# Purpose: assignment for week 10 - reader writer problem
 
-Instructions:
+# Instructions:
 
-- Review TODO comments
+# - Review TODO comments
 
-- writer: a process that will send numbers to the reader.  
-  The values sent to the readers will be in consecutive order starting
-  at value 1.  Each writer will use all of the sharedList buffer area
-  (ie., BUFFER_SIZE memory positions)
+# - writer: a process that will send numbers to the reader.  
+#   The values sent to the readers will be in consecutive order starting
+#   at value 1.  Each writer will use all of the sharedList buffer area
+#   (ie., BUFFER_SIZE memory positions)
 
-- reader: a process that receive numbers sent by the writer.  The reader will
-  accept values until indicated by the writer that there are no more values to
-  process.  
+# - reader: a process that receive numbers sent by the writer.  The reader will
+#   accept values until indicated by the writer that there are no more values to
+#   process.  
 
-- Do not use try...except statements
+# - Do not use try...except statements
 
-- Display the numbers received by the reader printing them to the console.
+# - Display the numbers received by the reader printing them to the console.
 
-- Create WRITERS writer processes
+# - Create WRITERS writer processes
 
-- Create READERS reader processes
+# - Create READERS reader processes
 
-- You can use sleep() statements for any process.
+# - You can use sleep() statements for any process.
 
-- You are able (should) to use lock(s) and semaphores(s).  When using locks, you can't
-  use the arguments "block=False" or "timeout".  Your goal is to make your
-  program as parallel as you can.  Over use of lock(s), or lock(s) in the wrong
-  place will slow down your code.
+# - You are able (should) to use lock(s) and semaphores(s).  When using locks, you can't
+#   use the arguments "block=False" or "timeout".  Your goal is to make your
+#   program as parallel as you can.  Over use of lock(s), or lock(s) in the wrong
+#   place will slow down your code.
 
-- You must use ShareableList between the two processes.  This shareable list
-  will contain different "sections".  There can only be one shareable list used
-  between your processes.
-  1) BUFFER_SIZE number of positions for data transfer. This buffer area must
-     act like a queue - First In First Out.
-  2) current value used by writers for consecutive order of values to send
-  3) Any indexes that the processes need to keep track of the data queue
-  4) Any other values you need for the assignment
+# - You must use ShareableList between the two processes.  This shareable list
+#   will contain different "sections".  There can only be one shareable list used
+#   between your processes.
+#   1) BUFFER_SIZE number of positions for data transfer. This buffer area must
+#      act like a queue - First In First Out.
+#   2) current value used by writers for consecutive order of values to send
+#   3) Any indexes that the processes need to keep track of the data queue
+#   4) Any other values you need for the assignment
 
-- Not allowed to use Queue(), Pipe(), List(), Barrier() or any other data structure.
+# - Not allowed to use Queue(), Pipe(), List(), Barrier() or any other data structure.
 
-- Not allowed to use Value() or Array() or any other shared data type from 
-  the multiprocessing package.
+# - Not allowed to use Value() or Array() or any other shared data type from 
+#   the multiprocessing package.
 
-- When each reader reads a value from the sharedList, use the following code to display
-  the value:
+# - When each reader reads a value from the sharedList, use the following code to display
+#   the value:
   
-                    print(<variable>, end=', ', flush=True)
+#                     print(<variable>, end=', ', flush=True)
 
-Add any comments for me:
+# Add any comments for me:
 
-"""
+# """
+# import random
+# from multiprocessing.managers import SharedMemoryManager
+# import multiprocessing as mp
+
+# BUFFER_SIZE = 10
+# READERS = 2
+# WRITERS = 2
+# SPECIAL_END_VALUE = -1
+
+# def writer(shared_list, empty_semaphore, filled_semaphore, access_lock, items_to_send, writer_id):
+#     current_item = 1
+#     while current_item <= items_to_send:
+#         empty_semaphore.acquire()
+#         with access_lock:
+#             tail = shared_list[BUFFER_SIZE + 2]
+#             shared_list[tail] = current_item
+#             shared_list[BUFFER_SIZE + 2] = (tail + 1) % BUFFER_SIZE
+#             current_item += 1
+#         filled_semaphore.release()
+
+#     for _ in range(READERS):
+#         empty_semaphore.acquire()
+#         with access_lock:
+#             tail = shared_list[BUFFER_SIZE + 2]
+#             shared_list[tail] = SPECIAL_END_VALUE
+#             shared_list[BUFFER_SIZE + 2] = (tail + 1) % BUFFER_SIZE
+#         filled_semaphore.release()
+
+# def reader(shared_list, empty_semaphore, filled_semaphore, access_lock, reader_id, read_count_index):
+#     read_count = 0
+#     end_count = 0
+#     while True:
+#         filled_semaphore.acquire()
+#         with access_lock:
+#             head = shared_list[BUFFER_SIZE + 1]
+#             item = shared_list[head]
+#             shared_list[BUFFER_SIZE + 1] = (head + 1) % BUFFER_SIZE
+#             if item == SPECIAL_END_VALUE:
+#                 end_count += 1
+#                 if end_count == WRITERS:
+#                     shared_list[read_count_index] = read_count
+#                     break
+#                 continue
+#             print(item, end=', ', flush=True)
+#             read_count += 1
+#         empty_semaphore.release()
+
+
+
+# def main():
+#     items_to_send = random.randint(1000, 10000)
+
+#     smm = SharedMemoryManager()
+#     smm.start()
+
+#     # TODO - Create a ShareableList to be used between the processes
+#     #      - The buffer should be size 10 PLUS at least three other
+#     #        values (ie., [0] * (BUFFER_SIZE + 3)).  The extra values
+#     #        are used for the head and tail for the circular buffer.
+#     #        The another value is the current number that the writers
+#     #        need to send over the buffer.  This last value is shared
+#     #        between the writers.
+#     #        You can add another value to the sharedable list to keep
+#     #        track of the number of values received by the readers.
+#     #        (ie., [0] * (BUFFER_SIZE + 4))
+#     read_count_indices = [BUFFER_SIZE + 4 + i for i in range(READERS)]
+#     shared_list = smm.ShareableList([0] * (BUFFER_SIZE + 4 + READERS))
+
+#     # TODO - Create any lock(s) or semaphore(s) that you feel you need
+#     empty_semaphore = mp.Semaphore(BUFFER_SIZE)
+#     filled_semaphore = mp.Semaphore(0)
+#     access_lock = mp.Lock()
+
+#     # TODO - create reader and writer processes
+#     writers = [mp.Process(target=writer, args=(shared_list, empty_semaphore, filled_semaphore, access_lock, items_to_send, i)) for i in range(WRITERS)]
+#     readers = [mp.Process(target=reader, args=(shared_list, empty_semaphore, filled_semaphore, access_lock, i, read_count_indices[i])) for i in range(READERS)]
+
+#     # TODO - Start the processes and wait for them to finish
+#     for w in writers:
+#         w.start()
+#     for r in readers:
+#         r.start()
+
+#     for w in writers:
+#         w.join()
+#     for r in readers:
+#         r.join()
+
+#     print(f'{items_to_send} values sent')
+
+#     # TODO - Display the number of numbers/items received by the reader.
+#     #        Can not use "items_to_send", must be a value collected
+#     #        by the reader processes.
+#     total_values_received = sum(shared_list[index] for index in read_count_indices)
+#     print(f'\n{items_to_send} values sent')
+#     print(f'Total values received: {total_values_received}')
+
+#     smm.shutdown()
+
+# if __name__ == '__main__':
+#     main()
+
+
 import random
 from multiprocessing.managers import SharedMemoryManager
 import multiprocessing as mp
@@ -103,37 +206,24 @@ def reader(shared_list, empty_semaphore, filled_semaphore, access_lock, reader_i
             read_count += 1
         empty_semaphore.release()
 
-
-
 def main():
     items_to_send = random.randint(1000, 10000)
 
     smm = SharedMemoryManager()
     smm.start()
 
-    # TODO - Create a ShareableList to be used between the processes
-    #      - The buffer should be size 10 PLUS at least three other
-    #        values (ie., [0] * (BUFFER_SIZE + 3)).  The extra values
-    #        are used for the head and tail for the circular buffer.
-    #        The another value is the current number that the writers
-    #        need to send over the buffer.  This last value is shared
-    #        between the writers.
-    #        You can add another value to the sharedable list to keep
-    #        track of the number of values received by the readers.
-    #        (ie., [0] * (BUFFER_SIZE + 4))
     read_count_indices = [BUFFER_SIZE + 4 + i for i in range(READERS)]
     shared_list = smm.ShareableList([0] * (BUFFER_SIZE + 4 + READERS))
+    shared_list[BUFFER_SIZE + 1] = 0  # Initialize head index
+    shared_list[BUFFER_SIZE + 2] = 0  # Initialize tail index
 
-    # TODO - Create any lock(s) or semaphore(s) that you feel you need
     empty_semaphore = mp.Semaphore(BUFFER_SIZE)
     filled_semaphore = mp.Semaphore(0)
     access_lock = mp.Lock()
 
-    # TODO - create reader and writer processes
     writers = [mp.Process(target=writer, args=(shared_list, empty_semaphore, filled_semaphore, access_lock, items_to_send, i)) for i in range(WRITERS)]
     readers = [mp.Process(target=reader, args=(shared_list, empty_semaphore, filled_semaphore, access_lock, i, read_count_indices[i])) for i in range(READERS)]
 
-    # TODO - Start the processes and wait for them to finish
     for w in writers:
         w.start()
     for r in readers:
@@ -144,11 +234,6 @@ def main():
     for r in readers:
         r.join()
 
-    print(f'{items_to_send} values sent')
-
-    # TODO - Display the number of numbers/items received by the reader.
-    #        Can not use "items_to_send", must be a value collected
-    #        by the reader processes.
     total_values_received = sum(shared_list[index] for index in read_count_indices)
     print(f'\n{items_to_send} values sent')
     print(f'Total values received: {total_values_received}')
--- week14/assignment/functions.py
+++ week14/assignment/functions.py
@@ -1,7 +1,7 @@
 """
 Course: CSE 251, week 14
 File: functions.py
-Author: <your name>
+Author: <jared linare>
 
 Instructions:
 
@@ -43,28 +43,38 @@ You will lose 10% if you don't detail your part 1 and part 2 code below
 
 Describe how to speed up part 1
 
-<Add your comments here>
+So in part one i decided to use a set which helped avoid some repeated processing. This really sped up the first part! 
+
+by using a set, it fixed some of the issues I had during coding, like 404s, which now rarely if ever occur. Additionally, it means that 
+the code will be able be more efficient
 
 
 Describe how to speed up part 2
 
-<Add your comments here>
+Using a queue helped manage the family processing, and did result in a 3 sec reduction in time from my old code to the current code.
 
 
 Extra (Optional) 10% Bonus to speed up part 3
 
-<Add your comments here>
+I tried to use a semaphore here, but kinda struggled, overall I tried, and I think i got it down to a reasonable time. 
 
 """
 from common import *
 import queue
+import threading
 
 # -----------------------------------------------------------------------------
-def depth_fs_pedigree(family_id, tree):
-    # KEEP this function even if you don't implement it
-    # TODO - implement Depth first retrieval
-    # TODO - Printing out people and families that are retrieved from the server will help debugging
-      # Request family details
+# by using the processed families argument i discovered i can more easily manage or keep track of family ids - seriously fixed so many issues
+# glad i went over some older notes and got this idea
+
+def depth_fs_pedigree(family_id, tree, processed_families=set()):
+# KEEP this function even if you don't implement it
+# TODO - implement Depth first retrieval
+# TODO - Printing out people and families that are retrieved from the server will help debugging
+    if family_id in processed_families:
+        return
+    processed_families.add(family_id)
+
     request = Request_thread(f'{TOP_API_URL}/family/{family_id}')
     request.start()
     request.join()
@@ -72,53 +82,50 @@ def depth_fs_pedigree(family_id, tree):
     family_data = request.get_response()
     if family_data:
         family = Family(family_data)
-        tree.add_family(family)  # Add the family to the tree
+        if not tree.does_family_exist(family.get_id()):
+            tree.add_family(family)
 
-        # Process husband and wife
-        if family.get_husband():
-            husband_request = Request_thread(f'{TOP_API_URL}/person/{family.get_husband()}')
-            husband_request.start()
-            husband_request.join()
-            husband = Person(husband_request.get_response())
-            tree.add_person(husband)  # Add the husband to the tree
+        person_ids = [family.get_husband(), family.get_wife()] + family.get_children()
+        person_threads = []
 
-        if family.get_wife():
-            wife_request = Request_thread(f'{TOP_API_URL}/person/{family.get_wife()}')
-            wife_request.start()
-            wife_request.join()
-            wife = Person(wife_request.get_response())
-            tree.add_person(wife)  # Add the wife to the tree
+        def process_person(person_id):
+            if person_id and not tree.does_person_exist(person_id):
+                person_request = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+                person_request.start()
+                person_request.join()
+                person_data = person_request.get_response()
+                if person_data:
+                    person = Person(person_data)
+                    tree.add_person(person)
 
-        # Process children
-        children_threads = []
-        for child_id in family.get_children():
-            child_request = Request_thread(f'{TOP_API_URL}/person/{child_id}')
-            child_request.start()
-            children_threads.append(child_request)
+                    parent_family_id = person.get_parentid()
+                    if parent_family_id and not tree.does_family_exist(parent_family_id):
+                        depth_fs_pedigree(parent_family_id, tree, processed_families)
 
-        for child_thread in children_threads:
-            child_thread.join()
-            child = Person(child_thread.get_response())
-            tree.add_person(child)  # Add each child to the tree
-
-            # Call depth_fs_pedigree for each child's family in a new thread
-            thread = threading.Thread(target=depth_fs_pedigree, args=(child.get_parentid(), tree))
+        for person_id in person_ids:
+            thread = threading.Thread(target=process_person, args=(person_id,))
+            person_threads.append(thread)
             thread.start()
-            thread.join() 
+
+        for thread in person_threads:
+            thread.join()
     pass
 
 # -----------------------------------------------------------------------------
 def breadth_fs_pedigree(family_id, tree):
-    # KEEP this function even if you don't implement it
-    # TODO - implement breadth first retrieval
-    # TODO - Printing out people and families that are retrieved from the server will help debugging
+# KEEP this function even if you don't implement it
+# TODO - implement breadth first retrieval
+# TODO - Printing out people and families that are retrieved from the server will help debugging
     family_queue = queue.Queue()
     family_queue.put(family_id)
+    processed_families = set()
 
     while not family_queue.empty():
         current_family_id = family_queue.get()
+        if current_family_id in processed_families:
+            continue
+        processed_families.add(current_family_id)
 
-        # Request family details
         family_request = Request_thread(f'{TOP_API_URL}/family/{current_family_id}')
         family_request.start()
         family_request.join()
@@ -126,57 +133,56 @@ def breadth_fs_pedigree(family_id, tree):
         family_data = family_request.get_response()
         if family_data:
             family = Family(family_data)
-            tree.add_family(family)  # Add the family to the tree
+            if not tree.does_family_exist(family.get_id()):
+                tree.add_family(family)
 
-            # Process husband and wife using appropriate methods from the Family class
-            if family.get_husband():
-                husband_request = Request_thread(f'{TOP_API_URL}/person/{family.get_husband()}')
-                husband_request.start()
-                husband_request.join()
-                husband = Person(husband_request.get_response())
-                tree.add_person(husband)
+            person_ids = [family.get_husband(), family.get_wife()] + family.get_children()
+            person_threads = []
 
-            if family.get_wife():
-                wife_request = Request_thread(f'{TOP_API_URL}/person/{family.get_wife()}')
-                wife_request.start()
-                wife_request.join()
-                wife = Person(wife_request.get_response())
-                tree.add_person(wife)
+            def process_person(person_id):
+                if person_id and not tree.does_person_exist(person_id):
+                    person_request = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+                    person_request.start()
+                    person_request.join()
+                    person_data = person_request.get_response()
+                    if person_data:
+                        person = Person(person_data)
+                        tree.add_person(person)
 
-            # Process children
-            children_threads = []
-            for child_id in family.get_children():
-                child_request = Request_thread(f'{TOP_API_URL}/person/{child_id}')
-                child_request.start()
-                children_threads.append(child_request)
+                        parent_family_id = person.get_parentid()
+                        if parent_family_id and parent_family_id != current_family_id:
+                            family_queue.put(parent_family_id)
 
-            for child_thread in children_threads:
-                child_thread.join()
-                child = Person(child_thread.get_response())
-                tree.add_person(child)
+            for person_id in person_ids:
+                thread = threading.Thread(target=process_person, args=(person_id,))
+                person_threads.append(thread)
+                thread.start()
 
-                # Add child's family ID to the queue for further processing
-                child_family_id = child.get_parentid()  # Adjust based on the Person class's method
-                if child_family_id and child_family_id != current_family_id:
-                    family_queue.put(child_family_id)
+            for thread in person_threads:
+                thread.join()
     pass
 
 # -----------------------------------------------------------------------------
-def breadth_fs_pedigree_limit5(family_id, tree):
-    # KEEP this function even if you don't implement it
-    # TODO - implement breadth first retrieval
-    #      - Limit number of concurrent connections to the FS server to 5
-    # TODO - Printing out people and families that are retrieved from the server will help debugging
+def breadth_fs_pedigree_limit5(start_family_id, tree):
+# KEEP this function even if you don't implement it
+# TODO - implement breadth first retrieval
+#      - Limit number of concurrent connections to the FS server to 5
+# TODO - Printing out people and families that are retrieved from the server will help debugging
     family_queue = queue.Queue()
-    family_queue.put(family_id)
+    family_queue.put((start_family_id, 1))
+    processed_families = set()
 
-    # Semaphore to limit the number of concurrent threads to 5
     semaphore = threading.Semaphore(5)
 
     while not family_queue.empty():
-        current_family_id = family_queue.get()
+        current_family_id, generation = family_queue.get()
 
-        def process_family(fid):
+        if current_family_id in processed_families:
+            continue
+
+        processed_families.add(current_family_id)
+
+        def process_family(fid, gen):
             with semaphore:
                 family_request = Request_thread(f'{TOP_API_URL}/family/{fid}')
                 family_request.start()
@@ -185,41 +191,24 @@ def breadth_fs_pedigree_limit5(family_id, tree):
                 family_data = family_request.get_response()
                 if family_data:
                     family = Family(family_data)
-                    tree.add_family(family)  # Add the family to the tree
+                    tree.add_family(family)
 
-                    # Process husband and wife using appropriate methods from the Family class
-                    if family.get_husband():
-                        husband_request = Request_thread(f'{TOP_API_URL}/person/{family.get_husband()}')
-                        husband_request.start()
-                        husband_request.join()
-                        husband = Person(husband_request.get_response())
-                        tree.add_person(husband)
+                    person_ids = [family.get_husband(), family.get_wife()] + family.get_children()
+                    for person_id in person_ids:
+                        if person_id and not tree.does_person_exist(person_id):
+                            person_request = Request_thread(f'{TOP_API_URL}/person/{person_id}')
+                            person_request.start()
+                            person_request.join()
+                            person_data = person_request.get_response()
+                            if person_data:
+                                person = Person(person_data)
+                                tree.add_person(person)
 
-                    if family.get_wife():
-                        wife_request = Request_thread(f'{TOP_API_URL}/person/{family.get_wife()}')
-                        wife_request.start()
-                        wife_request.join()
-                        wife = Person(wife_request.get_response())
-                        tree.add_person(wife)
+                                child_family_id = person.get_parentid()
+                                if child_family_id and child_family_id != fid:
+                                    family_queue.put((child_family_id, gen + 1))
 
-                    # Process children
-                    children_threads = []
-                    for child_id in family.get_children():
-                        child_request = Request_thread(f'{TOP_API_URL}/person/{child_id}')
-                        child_request.start()
-                        children_threads.append(child_request)
-
-                    for child_thread in children_threads:
-                        child_thread.join()
-                        child = Person(child_thread.get_response())
-                        tree.add_person(child)
-
-                        # Add child's family ID to the queue for further processing
-                        child_family_id = child.get_parentid()  # Adjust based on the Person class's method
-                        if child_family_id and child_family_id != fid:
-                            family_queue.put(child_family_id)
-
-        thread = threading.Thread(target=process_family, args=(current_family_id,))
+        thread = threading.Thread(target=process_family, args=(current_family_id, generation))
         thread.start()
-        thread.join()
+        thread.join()  
     pass
